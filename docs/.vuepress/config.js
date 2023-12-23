1d6b54de0643cfda90a032f44ada01a38fae7240
wanglei <wanglei311600@qq.com>
2023-12-24 02:10:52 +0800

    doc: fix config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,19 +12,9 @@ module.exports = {
   "themeConfig" : { 
       "logo" : "/images/logo.png",
       "nav" : [ 
-          { 
-            "link" : "/",
-            "text" : "首页"
-          },
-          { 
-            "items" : [ { 
-                  "link" : "/测试笔记/",
-                  "text" : "测试笔记"
-                } ],
-            "text" : "来自思源笔记"
-          }
         ],
-      "sidebar" : { "/测试笔记/" : [{title: '测试笔记', path: '/测试笔记/', children: [{title: '一层笔记1', path: '/测试笔记/一层笔记1/', children: [{title: '二层笔记1.1', path: '/测试笔记/一层笔记1/二层笔记1.1'}, {title: '二层笔记1.2', path: '/测试笔记/一层笔记1/二层笔记1.2'}]}, {title: '一层笔记2', path: '/测试笔记/一层笔记2/', children: [{title: '二层笔记2.1', path: '/测试笔记/一层笔记2/二层笔记2.1/', children: [{title: '三层笔记2.1.1', path: '/测试笔记/一层笔记2/二层笔记2.1/三层笔记2.1.1'}]}]}, {title: '一层笔记3', path: '/测试笔记/一层笔记3'}]}] },
+      "sidebar" : { 
+      },
       "sidebarDepth" : 2
     },
   "title" : "思源笔记"
